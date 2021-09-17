46d7cffcaefb1f8170f62256197b6196d3222482
s-omranpour <soroush.333@gmail.com>
2021-09-17 17:04:52 +0430

    added ssim suuport

--- src/model.py
+++ src/model.py
@@ -2,6 +2,9 @@ import torch
 from torch import nn
 import pytorch_lightning as pl
 from tqdm.notebook import tqdm
+from pytorch_msssim import SSIM, MS_SSIM
+
+
 from .modules import EncoderPrenet, DecoderPrenet, DecoderPostNet, RelativePositionalEncoding
 
 class Encoder(nn.Module):
@@ -10,8 +13,8 @@ class Encoder(nn.Module):
 
         self.alpha = nn.Parameter(torch.ones(1))
         self.pos_emb = RelativePositionalEncoding(d_hidden, max_len=20000)
-#         self.prenet = EncoderPrenet(n_vocab, d_emb, d_hidden, n_prenet_layers, kernel, dropout)
-        self.prenet = nn.Embedding(n_vocab, d_hidden)
+        self.prenet = EncoderPrenet(n_vocab, d_emb, d_hidden, n_prenet_layers, kernel, dropout)
+#         self.prenet = nn.Embedding(n_vocab, d_hidden)
         self.dropout = nn.Dropout(dropout)
         
         enc_layer = nn.TransformerEncoderLayer(
@@ -44,6 +47,7 @@ class Decoder(nn.Module):
         self.stop_linear = nn.Linear(d_hidden, 2)
 
         self.postnet = DecoderPostNet(n_mel, d_hidden, kernel, n_postnet_layers, dropout)
+#         self.postnet = nn.Linear(n_mel, n_mel)
 
 
     def forward(self, x, memory, att_mask=None, trg_length_mask=None, memory_length_mask=None):
@@ -56,8 +60,9 @@ class Decoder(nn.Module):
         n_time = x.shape[1]
         mel_out = self.mel_linear(x).view(n_batch, n_time * self.outputs_per_step, self.n_mel)
         post_out = (self.postnet(mel_out.transpose(1, 2)) + mel_out.transpose(1, 2)).transpose(1, 2)
+#         post_out = self.postnet(mel_out)
         stop_out = self.stop_linear(x)
-        return post_out, mel_out, stop_out
+        return mel_out, post_out, stop_out
 
 
 class TransformerTTS(pl.LightningModule):
@@ -67,16 +72,19 @@ class TransformerTTS(pl.LightningModule):
         self.audio_processor = audio_processor
         self.text_processor = text_processor
         self.l1 = nn.L1Loss(reduction='none')
-        self.l2 = nn.MSELoss(reduction='none')
+#         self.l2 = nn.MSELoss(reduction='none')
+#         self.ssim = SSIM(data_range=1, size_average=True, channel=1, nonnegative_ssim=True)
+#         self.ms_ssim = MS_SSIM(data_range=1, size_average=True, channel=1)
         self.ce = nn.CrossEntropyLoss(reduction='none', weight=torch.Tensor([0.8, 0.2]))
 
         self.encoder = Encoder(**config['encoder'])
         self.decoder = Decoder(**config['decoder'])
+            
 
     def configure_optimizers(self):
         opt = torch.optim.Adam(self.parameters(), lr=self.config['lr'])
-#         sch = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=self.config['max_epochs'], eta_min=0.)
-        return [opt]#, [sch]
+        sch = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=self.config['max_epochs'], eta_min=0.)
+        return [opt], [sch]
 
     def count_parameters(self):
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
@@ -101,24 +109,29 @@ class TransformerTTS(pl.LightningModule):
 
     def step(self, batch, batch_idx, mode='train'):
         characters, character_lengths, mels, mel_lengths = batch
-        post_out, mel_out, stop_out = self.forward(characters, mels, character_lengths, mel_lengths)
+        mel_out, post_out, stop_out = self.forward(characters, mels, character_lengths, mel_lengths)
         self.log('encoder alpha', self.encoder.alpha)
         self.log('decoder alpha', self.decoder.alpha)
         
         mask = 1-self._generate_length_mask(mel_lengths).to(mel_lengths.device).float()
         mask_2d = mask.unsqueeze(2).repeat(1,1,80)
+        
         l1_mel_loss = (self.l1(mel_out, mels) * mask_2d).mean()
-        l2_mel_loss = (self.l2(mel_out, mels) * mask_2d).mean()
         l1_post_loss = (self.l1(post_out, mels) * mask_2d).mean()
-        l2_post_loss = (self.l2(post_out, mels) * mask_2d).mean()
+#         l2_post_loss = (self.l2(post_out, mels) * mask_2d).mean()
+#         l2_mel_loss = (self.l2(mel_out, mels) * mask_2d).mean()
+#         ssim_mel_loss = self.ssim(mel_out.unsqueeze(1), mels.unsqueeze(1))
+#         ssim_post_loss = self.ssim(post_out.unsqueeze(1), mels.unsqueeze(1))
         stop_loss = (self.ce(stop_out.transpose(1,2), nn.functional.pad(mask, (0,1,0,0))[:,1:].long()) * mask).mean()
 
-        loss = l1_mel_loss + l1_post_loss + l2_mel_loss + l2_post_loss + stop_loss
+        loss = l1_mel_loss + l1_post_loss #+ ssim_mel_loss + ssim_post_loss + stop_loss + l2_post_loss + l2_mel_loss
 
         self.log(mode + '_l1_mel', l1_mel_loss.item())
         self.log(mode + '_l1_post', l1_post_loss.item())
-        self.log(mode + '_l2_mel', l2_mel_loss.item())
-        self.log(mode + '_l2_post', l2_post_loss.item())
+#         self.log(mode + '_ssim_mel', ssim_mel_loss.item())
+#         self.log(mode + '_ssim_post', ssim_post_loss.item())
+#         self.log(mode + '_l2_mel', l2_mel_loss.item())
+#         self.log(mode + '_l2_post', l2_post_loss.item())
         self.log(mode + '_stop_ce', stop_loss.item())
         self.log(mode + '_loss', loss.item())
         # if mode =='val':
@@ -152,9 +165,9 @@ class TransformerTTS(pl.LightningModule):
         mels = torch.zeros([1,1, 80])
         with torch.no_grad():
             for i in tqdm(range(max_len)):
-                post_out, mel_out, stopout = self.forward(prompt, mels)
+                mel_out, post_out, stop_out = self.forward(prompt, mels)
                 mels = torch.cat([mels, mel_out[:,-1:,:]], dim=1)
-                if torch.argmax(stopout[:, -1]) == 0:
+                if torch.argmax(stop_out[:, -1]) == 0:
                     break
                 
-        return mels[0], post_out[0]
+        return mels[0], post_out
